5bd893ef7ac84bf6647a6362066e2c73828ccc24
Reuben Joseph <reubenej@gmail.com>
2017-08-06 19:39:54 +0530

    Added system configuration script

---
+++ bootstrap_void.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+# Void setup script
+# Setup void linux environment and desktop
+#
+# Author: Reuben Joseph<reubenej@gmail.com>
+
+
+#=====================Message Colors=========================
+FAIL=$(tput setaf 1) #red
+PASS=$(tput setaf 2) #green
+HEAD=$(tput setaf 5) #magenta
+INFO=$(tput setaf 6) #cyan
+END=$(tput sgr0)   #ends color
+#============================================================
+
+
+#Print Heading  message
+# printf "${HEAD}<message>${END}\n"
+
+#Print informational message
+# printf "${INFO}<message>${END}\n"
+#
+#Print Failure message
+# printf "${FAIL}<message>${END}\n"
+#
+#Print Success message
+# printf "${PASS}<message>${END}\n"
+
+#Setup some variables
+USER=`whoami`
+PWD=`pwd`
+DESKTOP_ENVIRON=cinnamon
+
+#Install dev environment
+install_dev()
+{
+    printf "${HEAD}Toolchain Installation${END}\n"
+    printf "${INFO}Installing tools.${END}\n"
+    xbps-install -y gcc make git gdb vim tmux curl wget
+
+}
+
+install_desktop()
+{
+    printf "${HEAD}Desktop Installation${END}\n"
+    printf "${INFO}Synchronizing Repodata${END}\n"
+    xbps-install -S
+    printf "${INFO}Installing Xserver components${END}\n"
+
+    xbps-install -y xorg-minimal xorg-fonts xrandr xev
+
+    printf "${INFO}Installing Extra Fonts..${END}\n"
+    xbps-install -y font-inconsolata-otf ttf-ubuntu-font-family noto-fonts-ttf
+
+    printf "${INFO}Installing Desktop environment${END}\n"
+    xbps-install -y $DESKTOP_ENVIRON
+
+    #Install common applications
+    printf "${INFO}Installing Desktop applications${END}\n"
+    xbps-install -y parcellite sakura xarchiver firefox chromium mplayer
+}
+
+
+configure_vim()
+{
+    #Check if vim is installed
+    VIM=`vim`
+    #Setup pathogen
+
+
+}
